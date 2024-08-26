19681312070bbf7ff34d8e0101dccab589ec84b3
Vargas <jeanpaulfuentesvargas@gmail.com>
2024-08-26 12:15:23 -0600

    tarea subir

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  mode: 'development',
+  entry: {
+    'js/app' : './src/js/app.js',
+    'js/inicio' : './src/js/inicio.js',
+    'js/cliente/estadistica' : './src/js/cliente/estadistica.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public/build')
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+        filename: 'styles.css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(c|sc|sa)ss$/,
+        use: [
+            {
+                loader: MiniCssExtractPlugin.loader
+            },
+            'css-loader',
+            'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        type: 'asset/resource',
+      },
+    ]
+  }
+};
